f8a1c8f6b8118f316feaf14d1ee33f31f86c21c3
keonwooo <rjsdn7732@naver.com>
2024-08-31 12:37:08 +0900

    [2024.08.31] - 김건우 1. 첫 프로젝트 게시

---
+++ typeKeyB/src/main/java/com/kwfw/typeKeyB/TypeKeyBApplication.java
@@ -0,0 +1,13 @@
+package com.kwfw.typeKeyB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeKeyBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypeKeyBApplication.class, args);
+	}
+
+}
---
+++ typeKeyB/src/test/java/com/kwfw/typeKeyB/TypeKeyBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwfw.typeKeyB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeKeyBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

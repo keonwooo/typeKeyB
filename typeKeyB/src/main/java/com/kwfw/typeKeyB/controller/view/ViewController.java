0cef80529500fa367489e87e87ddcd6b587b145c
keonwooo <rjsdn7732@naver.com>
2024-09-01 11:31:18 +0900

    [2024,09.01] - 김건우 1. 첫 commit

---
+++ typeKeyB/src/main/java/com/kwfw/typeKeyB/controller/view/ViewController.java
@@ -0,0 +1,18 @@
+package com.kwfw.typeKeyB.controller.view;
+
+import org.springframework.stereotype.Controller;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class ViewController {
+	public String homeView(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return "/";
+	}
+}

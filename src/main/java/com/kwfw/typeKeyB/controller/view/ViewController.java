3e94709684862488caab30f10327ec6de0f77edb
keonwooo <rjsdn7732@naver.com>
2024-09-02 16:57:32 +0900

    [2024.09.02] - 김건우 1. html5 template 적용

--- src/main/java/com/kwfw/typeKeyB/controller/view/ViewController.java
+++ src/main/java/com/kwfw/typeKeyB/controller/view/ViewController.java
@@ -1,6 +1,7 @@
 package com.kwfw.typeKeyB.controller.view;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,8 +13,10 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Controller
 public class ViewController {
+	
+	@GetMapping("/")
 	public String homeView(HttpServletRequest request, HttpServletResponse response) throws Exception {
 //		test
-		return "/";
+		return "main";
 	}
 }
